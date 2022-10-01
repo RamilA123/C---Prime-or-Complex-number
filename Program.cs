eab892bb29fe0ee860cc71ef67a4378b003f6e3b
RamilA123 <ramilna@code.edu.az>
2022-10-01 21:32:58 +0400

    add prime or complex number file

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Prime_or_Complex_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 21, control = 1;
+
+            if (n <= 1)
+                Console.WriteLine("Enter the correct number.");
+            else
+            {
+                for (int i = 2; i < n; i++)
+                    if (n % i == 0)
+                    {
+                        control = 0;
+                        break;
+                    }
+
+                    if (control == 1)
+                        Console.WriteLine("It is the prime number.");
+                    else
+                        Console.WriteLine("It is the complex number.");           
+            }
+        }
+    }
+}
